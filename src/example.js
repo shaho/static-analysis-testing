23789cd0517db0e231ff9badb7f82b4fa583dd48
shaho <toofani@gmail.com>
2019-12-06 12:00:22 +0330

    Add eslint-config-prettier and handlers in script: format, check-format and validate

--- src/example.js
+++ src/example.js
@@ -1,7 +1,7 @@
 const name = "Freddy";
-typeof name === "strng";
+typeof name === "string";
 
-if (!"serviceWorker" in navigator) {
+if (!("serviceWorker" in navigator)) {
   // you have an old browser :-(
 }
 
